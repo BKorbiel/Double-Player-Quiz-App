b1748209844eb4083f77d4e50ed474a354decad0
Bartlomiej Korbiel <korbiel.bartek@gmail.com>
2023-05-03 16:41:07 +0200

    completed authentication

--- app/src/main/java/com/example/doubleplayerquizapp/LoginActivity.java
+++ app/src/main/java/com/example/doubleplayerquizapp/LoginActivity.java
@@ -1,14 +1,87 @@
 package com.example.doubleplayerquizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private TextInputEditText emailEdit, passwordEdit;
+    private Button loginBtn;
+    private ProgressBar progressBar;
+    private TextView goToRegistration;
+    private FirebaseAuth fbAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailEdit = findViewById(R.id.idEditEmail);
+        passwordEdit = findViewById(R.id.idEditPassword);
+        loginBtn = findViewById(R.id.idBtnLogin);
+        goToRegistration = findViewById(R.id.idGoToRegistration);
+        progressBar = findViewById(R.id.idProgressBar);
+        fbAuth = FirebaseAuth.getInstance();
+
+        goToRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = emailEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter your credentials", Toast.LENGTH_SHORT).show();
+                } else {
+                    fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(LoginActivity.this, "Successfully logged in.", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(i);
+                                finish();
+                            } else {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(LoginActivity.this, "Logging in failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = fbAuth.getCurrentUser();
+        if (user != null) {
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+            this.finish();
+        }
     }
 }
--- app/src/main/java/com/example/doubleplayerquizapp/RegistrationActivity.java
+++ app/src/main/java/com/example/doubleplayerquizapp/RegistrationActivity.java
@@ -1,14 +1,79 @@
 package com.example.doubleplayerquizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private TextInputEditText emailEdit, passwdEdit, cnfrmPasswdEdit;
+    private Button registerButton;
+    private ProgressBar progressBar;
+    private FirebaseAuth fbAuth;
+    private TextView goToLoginTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        emailEdit = findViewById(R.id.idEditEmail);
+        passwdEdit = findViewById(R.id.idEditPassword);
+        registerButton = findViewById(R.id.idBtnRegister);
+        cnfrmPasswdEdit = findViewById(R.id.idEditConfirmPassword);
+        progressBar = findViewById(R.id.idProgressBar);
+        fbAuth = FirebaseAuth.getInstance();
+        goToLoginTV = findViewById(R.id.idGoToLogin);
+        goToLoginTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = emailEdit.getText().toString();
+                String password = passwdEdit.getText().toString();
+                String confirmPassword = cnfrmPasswdEdit.getText().toString();
+
+                if (password.equals(cnfrmPasswdEdit) == false) {
+                    Toast.makeText(RegistrationActivity.this, "Passwords are not the same",Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegistrationActivity.this, "Enter your credentials",Toast.LENGTH_SHORT).show();
+                } else {
+                    fbAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(RegistrationActivity.this, "Registration completed",Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+                                startActivity(i);
+                                finish();
+                            } else {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(RegistrationActivity.this, "Registration failed",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
